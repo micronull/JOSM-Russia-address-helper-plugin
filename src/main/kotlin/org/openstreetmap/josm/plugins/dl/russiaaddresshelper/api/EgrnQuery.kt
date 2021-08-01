ff8d53b0b6e68014b73bf01362cdb96f41567354
Stepan I <2688692+De-Luxis@users.noreply.github.com>
2021-08-01 12:45:51 +0500

    Замена на Http1Client

--- src/main/kotlin/org/openstreetmap/josm/plugins/dl/russiaaddresshelper/api/EgrnQuery.kt
+++ src/main/kotlin/org/openstreetmap/josm/plugins/dl/russiaaddresshelper/api/EgrnQuery.kt
@@ -5,25 +5,20 @@ import org.openstreetmap.josm.data.projection.Projections
 import org.openstreetmap.josm.io.OsmTransferException
 import org.openstreetmap.josm.plugins.dl.russiaaddresshelper.RussiaAddressHelperPlugin
 import org.openstreetmap.josm.plugins.dl.russiaaddresshelper.io.EgrnSettingsReader
-import org.openstreetmap.josm.tools.HttpClient
+import org.openstreetmap.josm.tools.Http1Client
 import java.net.MalformedURLException
 import java.net.URL
 
 class EgrnQuery(private val coordinate: EastNorth) {
-    val httpClient: HttpClient
+    val httpClient: Http1Client
         get() {
-            return HttpClient.create(getUrl(), "GET")
-                .setAccept("application/json")
-                .setHeader("Content-Type", "application/json")
-                .setHeader("User-Agent", getUserAgent())
+            return Http1Client.create(getUrl(), "GET").setAccept("application/json").setHeader("Content-Type", "application/json").setHeader("User-Agent", getUserAgent()) as Http1Client
         }
 
     private fun getUrl(): URL {
         val mercator = Projections.getProjectionByCode("EPSG:3857")
         val projected = mercator.eastNorth2latlonClamped(coordinate)
-        val egrnUrlString = EgrnSettingsReader.EGRN_URL_REQUEST.get()
-            .replace("{lat}", projected.lat().toString())
-            .replace("{lon}", projected.lon().toString())
+        val egrnUrlString = EgrnSettingsReader.EGRN_URL_REQUEST.get().replace("{lat}", projected.lat().toString()).replace("{lon}", projected.lon().toString())
 
         return try {
             URL(egrnUrlString.replace(" ", "%20"))
@@ -34,8 +29,7 @@ class EgrnQuery(private val coordinate: EastNorth) {
 
     private fun getUserAgent(): String {
         return String.format(
-            "%s. Loading addresses for OpenStreetMaps.",
-            RussiaAddressHelperPlugin.versionInfo
-        );
+            "%s. Loading addresses for OpenStreetMaps.", RussiaAddressHelperPlugin.versionInfo
+        )
     }
 }
