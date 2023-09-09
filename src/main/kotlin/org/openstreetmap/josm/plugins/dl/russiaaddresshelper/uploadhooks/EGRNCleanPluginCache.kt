ed696a788dc358a41d62de6b4bb52dfc3301bc38
TakutoRU <takutoatmorgue@gmail.ru>
2023-09-09 11:49:22 +0300

    Fix clearing from EGRN service tags and objects on upload Fix README and CHANGELOG

--- src/main/kotlin/org/openstreetmap/josm/plugins/dl/russiaaddresshelper/uploadhooks/EGRNCleanPluginCache.kt
+++ src/main/kotlin/org/openstreetmap/josm/plugins/dl/russiaaddresshelper/uploadhooks/EGRNCleanPluginCache.kt
@@ -2,6 +2,7 @@ package org.openstreetmap.josm.plugins.dl.russiaaddresshelper.uploadhooks
 
 import org.openstreetmap.josm.actions.upload.UploadHook
 import org.openstreetmap.josm.data.APIDataSet
+import org.openstreetmap.josm.data.osm.OsmPrimitive
 import org.openstreetmap.josm.gui.MainApplication
 import org.openstreetmap.josm.plugins.dl.russiaaddresshelper.RussiaAddressHelperPlugin
 import org.openstreetmap.josm.tools.Logging
@@ -10,17 +11,14 @@ import org.openstreetmap.josm.tools.Logging
 class EGRNCleanPluginCache : UploadHook {
     override fun checkUpload(apiDataSet: APIDataSet): Boolean {
         val objectsToUpload = apiDataSet.primitives
-        var removedCount = 0;
+        var removedCount = 0
         objectsToUpload.forEach { primitive ->
             val egrnResult = RussiaAddressHelperPlugin.egrnResponses[primitive]
-            if (egrnResult != null && egrnResult.third.getPreferredAddress() != null) {
-                val preferredAddress = egrnResult.third.getPreferredAddress()!!
-                if (preferredAddress.getOsmAddress().getBaseAddressTagsWithSource()
-                        .all { primitive.hasTag(it.key, it.value) }
-                ) {
-                    RussiaAddressHelperPlugin.removeFromAllCaches(primitive)
-                    removedCount++
-                }
+            if (egrnResult != null && primitiveHasAddress(primitive)) { //выбираем примитивы, у которых по итогам редактирования заполнены адресные тэги
+                //возможно, стоит удалять так же и примитивы,
+                // для которых не был распознан адрес (условия?)
+                RussiaAddressHelperPlugin.removeFromAllCaches(primitive)
+                removedCount++
             }
         }
 
@@ -30,5 +28,9 @@ class EGRNCleanPluginCache : UploadHook {
         Logging.info("EGRN-PLUGIN Removed uploaded addresses from plugin validation cache ($removedCount)")
 
         return true
+    }
+
+    private fun primitiveHasAddress(primitive: OsmPrimitive): Boolean {
+        return primitive.hasTag("addr:housenumber") && (primitive.hasTag("addr:street") || primitive.hasTag("addr:place"))
     }
 }
--- src/main/kotlin/org/openstreetmap/josm/plugins/dl/russiaaddresshelper/uploadhooks/EGRNUploadTagFilter.kt
+++ src/main/kotlin/org/openstreetmap/josm/plugins/dl/russiaaddresshelper/uploadhooks/EGRNUploadTagFilter.kt
@@ -6,6 +6,7 @@ import org.openstreetmap.josm.command.DeleteCommand
 import org.openstreetmap.josm.command.SequenceCommand
 import org.openstreetmap.josm.data.APIDataSet
 import org.openstreetmap.josm.data.UndoRedoHandler
+import org.openstreetmap.josm.data.osm.Node
 import org.openstreetmap.josm.data.osm.OsmPrimitive
 import org.openstreetmap.josm.tools.I18n
 import org.openstreetmap.josm.tools.Logging
@@ -15,15 +16,18 @@ class EGRNUploadTagFilter : UploadHook {
     override fun checkUpload(apiDataSet: APIDataSet): Boolean {
         val objectsToUpload = apiDataSet.primitives
 
-        val needsToRemove = objectsToUpload.filter { it.hasTag("fixme", "REMOVE_ME!") }
+        val needsToRemove = objectsToUpload.filter {
+            it is Node && (it.hasTag("fixme", "REMOVE_ME!") ||
+                    (it.hasTag("fixme", "yes") && it.hasTag("source:addr", "ЕГРН")))
+        }
         if (needsToRemove.isNotEmpty()) {
             val removeObjects = SequenceCommand(
-                I18n.tr("Removed EGRN generated objects"),
+                I18n.tr("Removed EGRN generated nodes"),
                 DeleteCommand(needsToRemove)
             )
             UndoRedoHandler.getInstance().add(removeObjects)
             apiDataSet.removeProcessed(needsToRemove)
-            Logging.info("EGRN-PLUGIN Upload filter removed some unneeded objects (${needsToRemove.size})")
+            Logging.info("EGRN-PLUGIN Upload filter removed some unneeded nodes (${needsToRemove.size})")
         }
 
         val discardableKeys: Collection<String> = setOf(
@@ -32,7 +36,8 @@ class EGRNUploadTagFilter : UploadHook {
             "addr:RU:extracted_name",
             "addr:RU:extracted_type",
             "addr:RU:parsed_housenumber",
-            "addr:RU:parsed_flats"
+            "addr:RU:parsed_flats",
+            "egrn_name"
         )
         val needsChange = objectsToUpload.stream().flatMap { obj: OsmPrimitive -> obj.keys() }
             .anyMatch { o: String -> discardableKeys.contains(o) }
